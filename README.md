8e6c038901feb8d40d79d7a7fe53e7093f9a6525
yasunari.matsumoto <y-matsumoto@knit-itech.co.jp>
2024-08-03 05:26:37 +0000

    プロジェクト名を vegeket-> configに変更

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## 初回起動
 ```
 docker compose build
-docker compose run back django-admin startproject vegeket ./src
+docker compose run back django-admin startproject config ./src
 ```
 ※2回目以降は、`docker compose up -d` でOK。
 
 back: docker-compose.ymlファイルで定義されているbackサービスを指定しています。
-vegeket: 作成するプロジェクトの名前
+config: 作成するプロジェクトの名前
 ./src : カレントディレクトリ/srcにプロジェクトを作成することを指定
--- src/manage.py
+++ src/manage.py
@@ -3,16 +3,17 @@
 import os
 import sys
 
-from vegeket.logger import get_logger
+from config.logger import get_logger
 
-logger = get_logger('manage.py')
+logger = get_logger("manage.py")
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vegeket.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
-        
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -23,5 +24,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
